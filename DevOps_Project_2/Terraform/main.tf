806f745b2f2e67e117165730d99dbfadb31a25e9
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-05-19 00:48:56 +0530

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -1,19 +1,5 @@
-data "aws_ami" "amazon-linux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-resource "aws_instance" "dev_machine" {
-  ami = data.aws_ami.amazon-linux.id
+resource "aws_instance" "web-server" {
+  ami = ami-0a695f0d95cefc163
   instance_type = "t2.micro"
   key_name = "ansible"
 
