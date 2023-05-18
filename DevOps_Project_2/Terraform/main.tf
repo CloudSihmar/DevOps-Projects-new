531013db01f31e235cdd3e0f2d4f43441361ca4e
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-05-19 03:21:50 +0530

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web-server" {
   ami = "ami-08333bccc35d71140"
   instance_type = "t2.micro"
-  key_name = "ansible_key"
+  key_name = "ansible"
 
   tags = {
     Env = "test"
@@ -9,7 +9,7 @@ resource "aws_instance" "web-server" {
   }
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "ansible_key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDlnxkv0VJT/MNniLviGBE7h3wLuyxRw0TMO8GCfHG5GJh1s13r0fp26dx2vEce6bGz6c3v9rlkKsbx/DtGlX75QflJuxcfzFrpKg0C05wRPsKdtpt0QQEVcziKA1j75TU33gvHbgtgKOxiZOzcTXsAayoWZbhn9gkH1r41DKRZ5d2+yPJO0kg3rmSdRpdflGz0SBiYYVraUa2NFMQJgfkdQ5/JK+OLeWttRDyBJnwhlSUM/QCfSWA3QlnpfHs+0jzwlJiOXQmG3SA5HqMeZZQLlLLVi3/lw4bx6PNSuSgvmxTJrzqdvyUjubuUOPceKQi9CVlWKx9xFiz64d7aaktPy/i+1ZrGvRP6vbex2BGgRFgeXA0yqi/xdmLmJ09LnjNdKBMD3sGOi6Bjuw0tGHt8o0cvmoJsLsSoqg5VIqbXNAIs0tEPqLJtPOWss8MebzLKGMU63Td6s3dKxKsLQGmn37ru/fbFrZtispR5nFCNSe+cDvm52JirdnKqnWkHk0M= root@terra"
-}
+#resource "aws_key_pair" "deployer" {
+  #key_name   = "ansible_key"
+  #public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDlnxkv0VJT/MNniLviGBE7h3wLuyxRw0TMO8GCfHG5GJh1s13r0fp26dx2vEce6bGz6c3v9rlkKsbx/DtGlX75QflJuxcfzFrpKg0C05wRPsKdtpt0QQEVcziKA1j75TU33gvHbgtgKOxiZOzcTXsAayoWZbhn9gkH1r41DKRZ5d2+yPJO0kg3rmSdRpdflGz0SBiYYVraUa2NFMQJgfkdQ5/JK+OLeWttRDyBJnwhlSUM/QCfSWA3QlnpfHs+0jzwlJiOXQmG3SA5HqMeZZQLlLLVi3/lw4bx6PNSuSgvmxTJrzqdvyUjubuUOPceKQi9CVlWKx9xFiz64d7aaktPy/i+1ZrGvRP6vbex2BGgRFgeXA0yqi/xdmLmJ09LnjNdKBMD3sGOi6Bjuw0tGHt8o0cvmoJsLsSoqg5VIqbXNAIs0tEPqLJtPOWss8MebzLKGMU63Td6s3dKxKsLQGmn37ru/fbFrZtispR5nFCNSe+cDvm52JirdnKqnWkHk0M= root@terra"
+#}
