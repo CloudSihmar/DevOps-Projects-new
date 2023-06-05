a66da819e168fba13c290e7b4d54c996fd0102cb
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-06-06 00:42:41 +0530

    Update backend.tf

--- DevOps_Project_2/Terraform/backend.tf
+++ DevOps_Project_2/Terraform/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-ansible-jenkins-04199"
-    key = "server/statefile"
-    region = "us-west-1"
-  }
-}  
+#terraform {
+  #backend "s3" {
+    #bucket = "terraform-ansible-jenkins-04199"
+    #key = "server/statefile"
+    #region = "us-west-1"
+  #}
+#}  
